808cbd4b887e65d4ad9322ae004ec88b65e970f1
Kim Moberg <kimskogfelter@outlook.com>
2024-02-23 12:51:33 +0000

    apply changes made to make admin site work.. still doesnt work but need to take a break

--- main/settings.py
+++ main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'allauth',
     'allauth.account',
@@ -50,6 +51,9 @@ INSTALLED_APPS = [
 ]
 
 SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
