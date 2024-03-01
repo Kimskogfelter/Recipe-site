3e6c336f91ee2ba5cf9c326956b8cc4717ddbb86
Kim Moberg <kimskogfelter@outlook.com>
2024-03-01 10:43:15 +0000

    changed the layout with bootstrap on recipes site

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-kimskogfelte-recipesite-kj2l9ydts1i.ws-eu108.gitpod.io"]
 
