895aa52b4b1dcb785d8d72d2b5d9ac37f890cd6d
Kim Moberg <kimskogfelter@outlook.com>
2024-03-19 08:24:39 +0000

    changed the database in the settings.py file

--- main/settings.py
+++ main/settings.py
@@ -139,16 +139,16 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # DATABASES = {
-  #  'default': {
+  # 'default': {
    # 'ENGINE': 'django.db.backends.sqlite3',
-    #'NAME': BASE_DIR / 'db.sqlite3',
+    # 'NAME': BASE_DIR / 'db.sqlite3',
   # }
-# }
+ # }
 
 
 DATABASES = {
-'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+ 'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
