a3a5292a4638b35e81433826fa4567b684d6da23
Kim Moberg <kimskogfelter@outlook.com>
2024-03-22 15:27:40 +0000

    added code to the urls.py, forms.py for the contact app

---
+++ contact/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class ContactForm (forms.Form):
+    firstname = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "First name"}))
+    lastname = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Last name"}))
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={"placeholder": "Your e-mail"})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={"placeholder": "Your message"})
+    )
--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Contact
+from .views import Contact, SuccessForm
 
 
 urlpatterns = [
     path('', Contact.as_view(), name='contact')
+    path("successform/", SuccessForm.as_view(), name="successform"),
 ]
