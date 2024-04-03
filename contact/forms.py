d10eac33756c4f2affdd452eb435ce729281ae9b
Kim Moberg <kimskogfelter@outlook.com>
2024-04-03 09:45:09 +0000

    added the labels for the first and last name to the contact form in the forms.py

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class ContactUsForm (forms.Form):
-    firstname = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "First name"}))
-    lastname = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Last name"}))
+    firstname = forms.CharField(label='First Name', widget=forms.TextInput(attrs={"placeholder": "First name"}))
+    lastname = forms.CharField(label='Last Name', widget=forms.TextInput(attrs={"placeholder": "Last name"}))
     email = forms.EmailField(
         widget=forms.TextInput(attrs={"placeholder": "Your e-mail"})
     )
